9384776c807c367ce8512d3f7b1a94413e6f69ae
unknown <shreyas.kadlimatti9@gmail.com>
2024-09-18 04:17:15 -0400

    commit 1 in featurebranch

--- 2BL23CD041/prog1.c
+++ 2BL23CD041/prog1.c
@@ -2,4 +2,6 @@
 #include<stdio.h>
 void main()
 {printf("this is line 1 in master");
-printf("this is line 2 in master");}
+printf("this is line 2 in master");
+printf("this is line 1 in feature branch");
+}
